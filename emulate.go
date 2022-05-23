af3c76ef358979aa5b0f89a1cc2b72c8a1eba71d
il12 <il5498@yandex.ru>
2022-05-23 11:00:20 +0300

    Update emulate.go

--- emulate.go
+++ emulate.go
@@ -2,7 +2,7 @@ package chromedp
 
 import (
 	"github.com/chromedp/cdproto/emulation"
-	"github.com/chromedp/chromedp/device"
+	"github.com/il12/chromedp/device"
 )
 
 // EmulateAction are actions that change the emulation settings for the
